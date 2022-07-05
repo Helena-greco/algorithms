4a1eaad58d98502292e873bb59d811ced64691c1
Helena <nenafgreco@hotmail.com>
2022-07-04 23:56:25 -0400

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui """
+    count = 0
+    if target_time: 
+        for entry_time, exit_time in permanence_period:
+            if type(entry_time) != int or type(exit_time) != int:
+                return None
+            if entry_time <= target_time <= exit_time:
+                count += 1
+
+        return count
+    return None
