3d2acb4659ea0a73daa0df9e5622a4f0b4dc9a81
Helena <nenafgreco@hotmail.com>
2022-07-05 14:05:51 -0400

    refact requisito 1 para funcionar o 1.4

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    count = 0
-    if target_time:
+    try:
+        count = 0
         for entry_time, exit_time in permanence_period:
-            if type(entry_time) != int or type(exit_time) != int:
-                return None
             if entry_time <= target_time <= exit_time:
                 count += 1
-
         return count
-    return None
+    except TypeError:
+        return None
