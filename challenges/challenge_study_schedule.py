66a2c56d489864d76719a9ca307d3b3b158a198a
Helena <nenafgreco@hotmail.com>
2022-07-04 23:59:07 -0400

    corrigindo erros

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,6 +1,6 @@
 def study_schedule(permanence_period, target_time):
     count = 0
-    if target_time: 
+    if target_time:
         for entry_time, exit_time in permanence_period:
             if type(entry_time) != int or type(exit_time) != int:
                 return None
