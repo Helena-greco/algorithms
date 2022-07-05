de79aad85f71a67036162ab471e7c96f56f3a789
Helena <nenafgreco@hotmail.com>
2022-07-05 15:15:53 -0400

    requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,18 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first = list(first_string.lower())
+    second = list(second_string.lower())
+
+    for word in first:
+        if word in second:
+            second.remove(word)
+
+    if not first_string or not second_string:
+        return False
+
+    if len(second) == 0:
+        return True
+
+    else:
+        return False
+    
+# print(is_anagram(cat, act))
